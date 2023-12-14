e82622186f4be9a43093f989768757cd80d2d023
salmane10 <salmanelouham@gmail.com>
2023-12-14 23:47:46 +0100

    at night from home 14/12/23

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slouham <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: salman <salman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 14:55:18 by slouham           #+#    #+#             */
-/*   Updated: 2023/12/11 14:55:25 by slouham          ###   ########.fr       */
+/*   Updated: 2023/12/14 23:39:54 by salman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	dest_len = ft_strlen(dest);
 	src_len = ft_strlen(src);
 	total_len = dest_len + src_len;
-	if (size <= dest_len)
-		return (size + src_len);
+	if (!dest && size == 0)
+		return (src_len);
 	i = 0;
 	while (src[i] != '\0' && dest_len + i < size - 1)
 	{
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slouham <slouham@student.42.fr>            +#+  +:+       +#+        */
+/*   By: salman <salman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 14:54:56 by slouham           #+#    #+#             */
-/*   Updated: 2023/12/11 16:55:23 by slouham          ###   ########.fr       */
+/*   Updated: 2023/12/14 23:27:16 by salman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	size_t	i;
 
 	i = 0;
-	if (size <= 0)
-		return (ft_strlen(src));
-	while (src[i] && i < size - 1)
+	if (size > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
 	return (ft_strlen(src));
 }
